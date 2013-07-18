e3715b30a4ce207e4e1ba5142ae7db1d49fc7b63
Peter Tierno <peter.a.tierno@gmail.com>
2013-07-17 20:11:27 -0400

    changed ::SETTINGS.links from a hash to an array of arrays

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -5,7 +5,7 @@
 # Keep in mind that the head div is only 75px in hieght. Use an appropriate logo image.
 ::SETTINGS.logo_path   = 'http://www.example.com'
 ::SETTINGS.logo_image  = 'https://www.example.com/images/logo.png'
-::SETTINGS.links = {
-  'Test Link 1' => 'http://testlink1.com/',
-  'Test Link 2' => 'http://testlink2.com/',
-}
+::SETTINGS.links = [
+  [ 'Test Link 1', 'http://testlink1.com/' ],
+  [ 'Test Link 2', 'http://testlink2.com/' ],
+]
