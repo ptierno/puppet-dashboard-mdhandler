c4019eb39b2da22a51a8019f12f0157b669ad340
Peter Tierno <peter.a.tierno@gmail.com>
2013-07-19 13:30:40 -0400

    Changed shared nav view to make the link highlight when it is active

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-Registry.add_callback :core, :global_nav_widgets, "900_documentation" do |renderer|
+Registry.add_callback :core, :global_nav_widgets, "Documentation" do |renderer|
   renderer.render :partial => 'shared/doc_nav_widgets',
     :locals => { :link_name => 'Documentation', :my_link => '/documentation/'}
 end
